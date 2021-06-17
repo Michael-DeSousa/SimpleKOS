38fa74b7ea7a6e0256360bdda3f99d1027b4e23b
Michael De Sousa <michael.desous@gmail.com>
2021-06-16 18:58:11 -0700

    Basic tracking for player death + database setup

--- main.lua
+++ main.lua
@@ -2,10 +2,11 @@
 
 --Provides an addon object that can be referenced for all of AceAddon's calls. 
 --You can also add additional AceAddon libraries by referencing them here.
-local GankList = LibStub("AceAddon-3.0"):NewAddon("GankList", "AceConsole-3.0")
+local GankList = LibStub("AceAddon-3.0"):NewAddon("GankList", "AceConsole-3.0", "AceEvent-3.0")
 
 function GankList:OnInitialize()
-
+    self.db = LibStub("AceDB-3.0"):New("GankListDB")
+    GankList:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", "addToGankList")
 end
 
 function GankList:OnEnable()
@@ -14,4 +15,22 @@ end
 
 function GankList:OnDisable()
     GankList:Print("GankList disabled!")
+end
+
+function GankList:addToGankList()
+    local player = UnitGUID("player")
+    local _, subevent, _, sourceGUID, _, _, _, destGUID = CombatLogGetCurrentEventInfo()
+    if (destGUID == player) then
+        if (subevent == "SWING_DAMAGE") then
+            local overkill = select(13, CombatLogGetCurrentEventInfo())
+            if (overkill > 0) then
+                print(overkill)
+            end
+        elseif (subevent == "SPELL_DAMAGE" or subevent == "SPELL_PERIODIC_DAMAGE" or subevent == "RANGE_DAMAGE") then
+            local overkill = select(16, CombatLogGetCurrentEventInfo())
+            if (overkill > 0) then
+                print(overkill)
+            end
+        end           
+    end
 end
