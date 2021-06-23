e19729751e2eac7cdad38c66ddb8a150d053fe3b
Michael De Sousa <michael.desous@gmail.com>
2021-06-22 18:10:07 -0700

    Complete code rewrite + database functinality. Version 0.5

--- main.lua
+++ main.lua
@@ -1,12 +1,17 @@
 -- AceAddon3 quick start: https://www.wowace.com/projects/ace3/pages/getting-started
-
 --Provides an addon object that can be referenced for all of AceAddon's calls. 
 --You can also add additional AceAddon libraries by referencing them here.
 local GankList = LibStub("AceAddon-3.0"):NewAddon("GankList", "AceConsole-3.0", "AceEvent-3.0")
 
+local defaults = {
+    factionrealm = {
+        ganklist = {},
+    }
+}
+
 function GankList:OnInitialize()
     self.db = LibStub("AceDB-3.0"):New("GankListDB")
-    GankList:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", "checkIfGanked")
+    GankList:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", "checkForWorldPVP", defaults)
 end
 
 function GankList:OnEnable()
@@ -17,55 +22,167 @@ function GankList:OnDisable()
     GankList:Print("GankList disabled!")
 end
 
+--Given a player's GUID, will return a table containing the player's GUID, Name, Class, Race, Sex, and Realm
+function GankList:createPlayerRecord(playerGUID)
+    local newPlayerRecord = {}
+
+    newPlayerRecord.guid = playerGUID
+
+    --https://wowpedia.fandom.com/wiki/API_GetPlayerInfoByGUID
+    local _, tempClass, _, tempRace, tempSex, tempName, tempRealm = GetPlayerInfoByGUID(playerGUID)
+
+    newPlayerRecord.name = tempName
+
+    --Creating records with the localization-independent return values to stay consistent
+    newPlayerRecord.class = string.lower(tempClass)
+    newPlayerRecord.race = string.lower(tempRace)
+
+    if tempSex == 2 then
+        newPlayerRecord.sex = "male"
+    elseif tempSex == 3 then
+        newPlayerRecord.sex = "female"
+    else
+        newPlayerRecord.sex = "neutral"
+    end
+
+    if tempRealm == "" then
+        newPlayerRecord.realm = GetRealmName()
+    end
+
+    return newPlayerRecord
+end
+
+--Table of hostile players that have directly attacked the player (Melee attack, spell, DoT, Debuffs, etc.)
+--Each entry is a record created using createPlayerRecord() and the player's GUID is used as a unique key
+local recentAttackers = {}
+
+--Given a player GUID, creates a player record and appends it to the recentAttackers list
+function GankList:addToRecentAttackers(attackerGUID)
+    print("Called addToRecentAttackers")
+    local newAttackerRecord = GankList:createPlayerRecord(attackerGUID)
+    print("Record created. GUID is " .. newAttackerRecord.guid)
+    recentAttackers[newAttackerRecord.guid] = newAttackerRecord
+end
+
+--Given a player GUID, either returns the matching player record from recentAttackers, or returns nil if it does not exist
+function GankList:getRecentAttackerRecord(playerGUID)
+    --Don't need to loop, just check if the key playerGUID is not nil aka if recentAttackers[playerGUID] ~= nil
+    print("Called getRecentAttackerRecord")
+    for attackerGUID, attackerRecord in pairs(recentAttackers) do
+        if attackerGUID == playerGUID then
+            print("Record was found in recentAttackers table for " .. attackerRecord.name)
+            return attackerRecord
+        end
+    end
+end
+
 --https://www.wowinterface.com/forums/showthread.php?t=49974
---The purpose of this tooltip is simply to give us a way to 
+--The purpose of this scanner is to give us a way to grab a hostile player's name when their pet attacks the player. 
+--After finding the forum post above this seems like a reasonable way to get what we need
 local petScanner = CreateFrame("GameTooltip", "PetScanner", nil, "GameTooltipTemplate")
-local function getPetOwnerGUID(guid)
-    --Calls function when referenced?
-    local text = _G["PetScannerTextLeft2"]
-    if(guid and text) then
-        petScanner:SetOwner( WorldFrame, "ANCHOR_NONE" )
-        --Set hyperlink will make the tooltip switch to display our chosen GUID
-        petScanner:SetHyperlink(format('unit:%s',guid))
-        --Calling Text then calls the function to get the object, calling getText then gives us the string!! smart
-        local ownerText = text:GetText()
+function GankList:getPetOwnerName(petGUID)
+
+    local tooltipText = _G["PetScannerTextLeft2"]
+    if petGUID and tooltipText then
+        petScanner:SetOwner(WorldFrame, "ANCHOR_NONE")
+        petScanner:SetHyperlink(format('unit:%s', petGUID))
+
+        local ownerText = tooltipText:GetText()
         local ownerName, _ = string.split("'", ownerText)
         return ownerName
     end
 end
 
+--Given a pet's GUID, we call getPetOwnerName to get the name of its owner. We then search through our list of recent attackers to see if we can find a match and get information on the owner
+--Note: If the pet owner never directly attacks the player, then they won't be on the recentAttackers table and we won't be able to get their information.  
+function GankList:getPetOwnerRecord(petGUID) 
+    local ownerName = GankList:getPetOwnerName(petGUID)
+    if(ownerName) then
+        for _, attackerRecord in pairs(recentAttackers) do
+            if attackerRecord.name == ownerName then
+                return attackerRecord
+            end
+        end
+    end
+end
+
+--Given a player's GUID, we return true if there is a player record for that GUID in the GankListDB or false if there is not.
+function GankList:isInGanklist(playerGUID)
+    print("Called isInGanklist")
+    if self.db.factionrealm.ganklist[playerGUID] ~= nil then
+        print("The player GUID was found in the gank list")
+        return true
+    end
+    return false
+end
+
+--Adds a given player record to the GankListDB with the record's GUID as the key. 
+--Records added to the GankListDB will be saved to the player's "Saved Variables" folder and persist after logout.
+function GankList:addToGankList(playerRecord)
+    print("Called addToGanklist")
+    if playerRecord.guid and playerRecord.name and playerRecord.class and playerRecord.sex and playerRecord.race then
+        print("Valid player record, adding to list")
+        self.db.factionrealm.ganklist[playerRecord.guid] = playerRecord
+    else
+        GankList:Print("Invalid player record, cannot add to the ganklist.")
+    end
+end
+
+--https://wowpedia.fandom.com/wiki/API_CombatLog_Object_IsA
 local COMBATLOG_FILTER_HOSTILE_PLAYERS = 0x7D4E
 local COMBATLOG_FILTER_HOSTILE_PLAYER_PET = 0x1148
-local COMBATLOG_FILTER_ME = 0x0511
+local COMBATLOG_FILTER_PLAYER = 0x0511
 
-function GankList:checkIfGanked()
+function GankList:checkForWorldPVP()
     if not IsInInstance() then
-        local _, subevent, _, sourceGUID, sourceName, sourceFlags, _, destGUID, destName, destFlags = CombatLogGetCurrentEventInfo()
-        if CombatLog_Object_IsA(sourceFlags, COMBATLOG_FILTER_HOSTILE_PLAYERS) and CombatLog_Object_IsA(destFlags, COMBATLOG_FILTER_ME) then
-            local enemyPlayer = {}
+        local _, subevent, _, attackerGUID, attackerName, attackerFlags, _, victimGUID, victimName, victimFlags = CombatLogGetCurrentEventInfo()
 
-            if CombatLog_Object_IsA(sourceFlags, COMBATLOG_FILTER_HOSTILE_PLAYER_PET) then
-                print("EVENT FROM PLAYER PET CALLED: " .. sourceName)
-                enemyPlayer.name = getPetOwner(sourceGUID)
-                enemyplayer.class = "Unknown"
-                enemyplayer.race = "Unknown"
-                enemyplayer.sex = "Unknown"
-                enemyplayer.realm = ""
-            else
-                _, enemyPlayer.class, _, enemyPlayer.race, enemyPlayer.sex, enemyPlayer.name, enemyPlayer.realm = GetPlayerInfoByGUID(sourceGUID)
+        if CombatLog_Object_IsA(attackerFlags, COMBATLOG_FILTER_HOSTILE_PLAYERS) and CombatLog_Object_IsA(victimFlags, COMBATLOG_FILTER_PLAYER) then            
+            local attackerRecord
+            if not CombatLog_Object_IsA(attackerFlags, COMBATLOG_FILTER_HOSTILE_PLAYER_PET) then
+                print("Not a pet attacking...")
+                attackerRecord = GankList:getRecentAttackerRecord(attackerGUID)
+                if attackerRecord == nil then
+                    print("Attacker record for " .. attackerName .. "does not exist")
+                    GankList:addToRecentAttackers(attackerGUID)
+                    attackerRecord = GankList:getRecentAttackerRecord(attackerGUID)
+                    print("Added " .. attackerRecord.name .. " to the recent attackers list")
+                end
             end
 
+            --https://wowpedia.fandom.com/wiki/COMBAT_LOG_EVENT
+            local overkill
             if (subevent == "SWING_DAMAGE") then
-                local overkill = select(13, CombatLogGetCurrentEventInfo())                        
-                if (overkill > 0) then
-                    print(enemyPlayer.name .." the " .. enemyPlayer.sex .. " " .. enemyPlayer.race .. " " .. enemyPlayer.class .. " killed you.")
-                end
+                overkill = select(13, CombatLogGetCurrentEventInfo())
             elseif (subevent == "SPELL_DAMAGE") or (subevent == "SPELL_PERIODIC_DAMAGE") or (subevent == "RANGE_DAMAGE") then
-                local overkill = select(16, CombatLogGetCurrentEventInfo())
-                if (overkill > 0) then
-                    print(enemyPlayer.name .." the " .. enemyPlayer.sex .. " " .. enemyPlayer.race .. " " .. enemyPlayer.class .. " killed you.")
+                overkill = select(16, CombatLogGetCurrentEventInfo())
+            end
+
+            if (overkill >= 0) then
+                --If the player is killed by a pet, we don't have an easy to to get a hostile pet owner's name using a petUID. See comments in the pet owner functions
+                if CombatLog_Object_IsA(attackerFlags, COMBATLOG_FILTER_HOSTILE_PLAYER_PET) then 
+                    attackerRecord = GankList:getPetOwnerRecord(attackerGUID)
+                    if attackerRecord == nil then
+                        GankList:Print("You were Killed by " .. GankList:getPetOwnerName(attackerGUID) .. "'s pet but GankList could not scan the player's information. Please add the player to the list manually.")
+                    else
+                        print("Killed by pet belonging to " .. attackerRecord.name)
+                        if not GankList:isInGanklist(attackerRecord.guid) then
+                            print("PetOwner not in ganklist, adding...")
+                            GankList:addToGanklist(attackerRecord)
+                        else
+                            print("Already on ganklist")
+                        end
+                    end             
+                else
+                    print("Killed by " .. attackerRecord.name)
+                    if not GankList:isInGanklist(attackerRecord.guid) then
+                        print("Not in ganklist, adding...")
+                        GankList:addToGankList(attackerRecord)
+                    else
+                        print("Already on ganklist")
+                    end
                 end
-            end     
+            end
         end
     end
 end
